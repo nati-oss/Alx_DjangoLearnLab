5784ff987b5f9b84999cbcaababf139a6ff54435
xilak <natanzerihun101@gmail.com>
2025-09-14 22:24:16 +0300

    task 1

--- advanced_features_and_security/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-
-      "relationship_app",
+    "relationship_app",
+    'LibraryProject.bookshelf',
 ]
 
 MIDDLEWARE = [
