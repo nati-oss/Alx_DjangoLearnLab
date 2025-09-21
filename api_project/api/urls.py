af694c53ce5f06352b6dd7ddaa4cd855c972eae7
xilak <natanzerihun101@gmail.com>
2025-09-21 14:41:35 +0300

    week 12 2

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Old ListAPIView endpoint
     path('books/', BookList.as_view(), name='book-list'),
+
+    # New ViewSet endpoints
+    path('', include(router.urls)),
 ]
