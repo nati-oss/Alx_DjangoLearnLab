a25036a832f560187c045c84eae0abc8c76826b5
xilak <natanzerihun101@gmail.com>
2025-09-21 15:15:15 +0300

    week 12 3

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,3 +9,4 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # 👈 Only logged-in users with token
